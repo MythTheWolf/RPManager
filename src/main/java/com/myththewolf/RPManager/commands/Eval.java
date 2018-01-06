3cdb99c872321478efcee1c2dc021590621c1205
MythTheWolf <myth.wolf6@gmail.com>
2018-01-06 02:10:51 -0600

    Implement eval

--- src/main/java/com/myththewolf/RPManager/commands/Eval.java
+++ src/main/java/com/myththewolf/RPManager/commands/Eval.java
@@ -4,11 +4,13 @@ import com.myththewolf.BotServ.lib.API.command.CommandExecutor;
 import com.myththewolf.BotServ.lib.API.command.DiscordCommand;
 import com.myththewolf.RPManager.RPManagerLoader;
 import com.myththewolf.RPManager.lib.User.DiscordUser;
+import net.dv8tion.jda.core.EmbedBuilder;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import java.awt.*;
 
-public class Eval implements CommandExecutor{
+public class Eval implements CommandExecutor {
     @Override
     public void onCommand(DiscordCommand discordCommand) {
         try {
@@ -16,8 +18,19 @@ public class Eval implements CommandExecutor{
             ScriptEngine engine = manager.getEngineByName("js");
             engine.put("con", RPManagerLoader.getSQLConnection());
             DiscordUser dd = null;
-            engine.put("user",dd);
-        }catch (Exception e){
+            engine.put("user", dd);
+            String bb = "";
+            for (String S : discordCommand.getArgs()) {
+                bb += S + " ";
+            }
+            Object res = engine.eval(bb);
+            EmbedBuilder EB = new EmbedBuilder();
+            EB.setTitle("Java Evalutation");
+            EB.setColor(Color.BLACK);
+            EB.addField(":printer: Result", "```" + res.toString() + "```", false);
+            EB.addField(":wrench: ResultType", "```" + res.getClass() + "```", false);
+            discordCommand.reply(EB);
+        } catch (Exception e) {
             RPManagerLoader.LogError(e);
         }
     }
