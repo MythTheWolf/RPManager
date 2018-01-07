cb6acece9d7e845898efc75d39b8929b0e5a1c7e
MythTheWolf <myth.wolf6@gmail.com>
2018-01-07 05:54:05 -0600

    Debug

--- src/main/java/com/myththewolf/RPManager/lib/Services/LastPostCheckService.java
+++ src/main/java/com/myththewolf/RPManager/lib/Services/LastPostCheckService.java
@@ -23,7 +23,8 @@ public class LastPostCheckService implements Runnable {
                 warning.addField("RP name", "```" + rp.getRoleplayName() + "```", false);
                 warning.addField("RP Character name:", "```" + rp.getStagedCharacter().getName() + "```", false);
                 warning.setFooter("_You will be pinged every 16 hours, and you will lose 4 reputation every 16 hours until you reply._", null);
-                rp.getStagedCharacter().getCharacterOwner().asPrivateChannel().sendMessage(warning.build()).queue();
+                // rp.getStagedCharacter().getCharacterOwner().asPrivateChannel().sendMessage(warning.build()).queue();
+                System.out.println(hoursSinceLastPing);
                 rp.setLastPing(now);
             }
         });
