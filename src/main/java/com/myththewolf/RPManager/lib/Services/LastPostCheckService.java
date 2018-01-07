14d0dc6d97aa613ecaae237097f1ad01550aae19
MythTheWolf <myth.wolf6@gmail.com>
2018-01-07 05:40:42 -0600

    Update service

--- src/main/java/com/myththewolf/RPManager/lib/Services/LastPostCheckService.java
+++ src/main/java/com/myththewolf/RPManager/lib/Services/LastPostCheckService.java
@@ -11,8 +11,12 @@ public class LastPostCheckService implements Runnable {
             DateTime lastPost = rp.getLastPostDate();
             DateTime lastPing = rp.getLastPing();
             DateTime now = new DateTime();
-            int hours = Hours.hoursBetween(lastPost,now).getHours();
-            System.out.println("h"+hours);
+            int hoursSinceLastPost = Hours.hoursBetween(lastPost, now).getHours();
+            int hoursSinceLastPing = lastPing != null ? Hours.hoursBetween(lastPing, now).getHours() : 17;
+            if (hoursSinceLastPing >= 16 && hoursSinceLastPost >= 16) {
+                System.out.println("SHUD_POST");
+                rp.getStagedCharacter().getCharacterOwner().asPrivateChannel().sendMessage("HU").queue();
+            }
         });
     }
 }
