e9b53ae9c2b2c799f074980a9f07f3fc33374a8d
MythTheWolf <myth.wolf6@gmail.com>
2018-01-07 05:36:47 -0600

    suische

--- src/main/java/com/myththewolf/RPManager/lib/Services/LastPostCheckService.java
+++ src/main/java/com/myththewolf/RPManager/lib/Services/LastPostCheckService.java
@@ -1,8 +1,18 @@
 package com.myththewolf.RPManager.lib.Services;
 
-public class LastPostCheckService implements Runnable{
+import com.myththewolf.RPManager.lib.DataCache;
+import org.joda.time.DateTime;
+import org.joda.time.Hours;
+
+public class LastPostCheckService implements Runnable {
     @Override
     public void run() {
-        System.out.println("CHECK");
+        DataCache.getRoleplayMap().forEach((id, rp) -> {
+            DateTime lastPost = rp.getLastPostDate();
+            DateTime lastPing = rp.getLastPing();
+            DateTime now = new DateTime();
+            int hours = Hours.hoursBetween(lastPost,now).getHours();
+            System.out.println("h"+hours);
+        });
     }
 }
