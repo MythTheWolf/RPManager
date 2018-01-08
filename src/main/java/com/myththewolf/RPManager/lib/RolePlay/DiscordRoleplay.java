c82c6e88808e031df5d2c63a57d11ac2cf9b9feb
MythTheWolf <myth.wolf6@gmail.com>
2018-01-08 05:39:59 -0600

    Update the status when recompiling a roleplay

--- src/main/java/com/myththewolf/RPManager/lib/RolePlay/DiscordRoleplay.java
+++ src/main/java/com/myththewolf/RPManager/lib/RolePlay/DiscordRoleplay.java
@@ -114,13 +114,14 @@ public class DiscordRoleplay {
 
     public void recompile() {
         try {
-            PreparedStatement up = RPManagerLoader.getSQLConnection().prepareStatement("UPDATE `Roleplays` SET `expire_date` = ?, `name` = ?, `last_post_date` = ?, `character_ids` = ?, `turn` = ?  WHERE `ID` = ?");
+            PreparedStatement up = RPManagerLoader.getSQLConnection().prepareStatement("UPDATE `Roleplays` SET `expire_date` = ?, `name` = ?, `last_post_date` = ?, `character_ids` = ?, `turn` = ?, `status` = ?  WHERE `ID` = ?");
             up.setString(1, DateTimeFormat.forPattern(DataCache.SYSTEM_DATE_FORMAT).print(getExpireDate()));
             up.setString(2, getRoleplayName());
             up.setString(3, DateTimeFormat.forPattern(DataCache.SYSTEM_DATE_FORMAT).print(getLastPostDate()));
             up.setString(4, getSerializedCharacterString());
             up.setInt(5, turn);
-            up.setInt(6, getId());
+            up.setString(6, getStatus());
+            up.setInt(7, getId());
             up.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
