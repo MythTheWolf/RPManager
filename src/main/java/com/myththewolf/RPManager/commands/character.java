f7b8740a443f27501021617ea52634b6cf2aa82b
MythTheWolf <myth.wolf6@gmail.com>
2018-01-07 21:50:18 -0600

    Fixed stuff

--- src/main/java/com/myththewolf/RPManager/commands/character.java
+++ src/main/java/com/myththewolf/RPManager/commands/character.java
@@ -2,16 +2,20 @@ package com.myththewolf.RPManager.commands;
 
 import com.myththewolf.BotServ.lib.API.command.CommandExecutor;
 import com.myththewolf.BotServ.lib.API.command.DiscordCommand;
+import com.myththewolf.RPManager.RPManagerLoader;
 import com.myththewolf.RPManager.lib.Character.RolePlayCharacter;
 import com.myththewolf.RPManager.lib.DataCache;
 import com.myththewolf.RPManager.lib.DiscordUtils;
+import com.myththewolf.RPManager.lib.User.DiscordUser;
 import net.dv8tion.jda.core.EmbedBuilder;
 
 
 import java.awt.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class character implements CommandExecutor {
     @Override
@@ -22,40 +26,47 @@ public class character implements CommandExecutor {
         }
 
         if (isInt(discordCommand.getArgs()[0])) {
-            List<RolePlayCharacter> possible = new ArrayList<>();
-            DataCache.getUserMap().forEach((key, val) -> {
-                val.getCharacters().stream().filter(character -> character.getID() == Integer.parseInt(discordCommand.getArgs()[0])).collect(Collectors.toList()).forEach(d -> {
-                    possible.add(d);
-                });
-            });
-            if (possible.size() < 1) {
-                discordCommand.reply("nun");
-                return;
-            } else {
-                EmbedBuilder reply = new EmbedBuilder();
-                RolePlayCharacter c = possible.get(0);
-                reply.setColor(Color.GREEN);
-                reply.setTitle("Character Info - " + c.getName());
-                reply.addField("Name", "```" + c.getName() + "```", false);
-                reply.addField("Species", "```" + c.getSpecies() + "```", false);
-                reply.addField("Height", "```" + c.getHeight() + "```", false);
-                reply.addField("Gender/Pronouns", "```" + c.getGender() + "```", false);
-                reply.addField("Color patterns", "```" + c.getColors() + "```", false);
-                reply.addField("Sexual Preference", "```" + c.getSexuality() + "```", false);
-                if (c.getReferenceImages().size() < 1) {
-                    reply.setThumbnail("https://vignette.wikia.nocookie.net/janethevirgin/images/4/42/Image-not-available_1.jpg/revision/latest?cb=20150721102313");
-                } else {
-                    reply.setThumbnail(c.getReferenceImages().get(0));
+            boolean didthedo = false;
+            RolePlayCharacter c = null;
+            try {
+                PreparedStatement ps = RPManagerLoader.getSQLConnection().prepareStatement("SELECT * FROM `Characters` WHERE `ID` = ?");
+                ps.setInt(1, Integer.parseInt(discordCommand.getArgs()[0]));
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    didthedo = true;
+                    c = new RolePlayCharacter(Integer.parseInt(discordCommand.getArgs()[0]));
                 }
-                String bottom_build = "";
-                bottom_build += c.getBio() + "\n";
-                bottom_build += "**Additional Notes**+\n";
-                bottom_build += "```" + c.getNotes() + "```\n";
-                reply.setDescription(bottom_build);
-                discordCommand.e.getTextChannel().sendMessage(reply.build()).queue();
+                if (!didthedo) {
+                    discordCommand.reply(":warning: No character found by that ID");
+                    return;
+                }
+            } catch (SQLException e) {
+                RPManagerLoader.LogError(e);
             }
+            EmbedBuilder reply = new EmbedBuilder();
+            reply.setColor(Color.GREEN);
+            reply.setTitle("Character Info - " + c.getName());
+            reply.addField("Name", "```" + c.getName() + "```", false);
+            reply.addField("Species", "```" + c.getSpecies() + "```", false);
+            reply.addField("Height", "```" + c.getHeight() + "```", false);
+            reply.addField("Gender/Pronouns", "```" + c.getGender() + "```", false);
+            reply.addField("Color patterns", "```" + c.getColors() + "```", false);
+            reply.addField("Sexual Preference", "```" + c.getSexuality() + "```", false);
+            if (c.getReferenceImages().size() < 1) {
+                reply.setThumbnail("https://vignette.wikia.nocookie.net/janethevirgin/images/4/42/Image-not-available_1.jpg/revision/latest?cb=20150721102313");
+            } else {
+                reply.setThumbnail(c.getReferenceImages().get(0));
+            }
+            String bottom_build = "";
+            bottom_build += c.getBio() + "\n";
+            bottom_build += "**Additional Notes**+\n";
+            bottom_build += "```" + c.getNotes() + "```\n";
+            reply.setDescription(bottom_build);
+            discordCommand.e.getTextChannel().sendMessage(reply.build()).queue();
         }
     }
+
 
     boolean isInt(String test) {
         try {
