dc7ac3d9e703a1e00d9063c6e6b71a26e69c07e3
MythTheWolf <myth.wolf6@gmail.com>
2018-01-08 01:05:48 -0600

    switch params to match correctly

--- src/main/java/com/myththewolf/RPManager/lib/RolePlay/DiscordRoleplay.java
+++ src/main/java/com/myththewolf/RPManager/lib/RolePlay/DiscordRoleplay.java
@@ -106,8 +106,8 @@ public class DiscordRoleplay {
             PreparedStatement up = RPManagerLoader.getSQLConnection().prepareStatement("UPDATE `Roleplays` SET `expire_date` = ?, `name` = ?, `last_post_date` = ?, `character_ids` = ?  WHERE `ID` = ?");
             up.setString(1, DateTimeFormat.forPattern(DataCache.SYSTEM_DATE_FORMAT).print(getExpireDate()));
             up.setString(2, getRoleplayName());
-            up.setString(3, getSerializedCharacterString());
-            up.setString(4, DateTimeFormat.forPattern(DataCache.SYSTEM_DATE_FORMAT).print(getLastPostDate()));
+            up.setString(3, DateTimeFormat.forPattern(DataCache.SYSTEM_DATE_FORMAT).print(getLastPostDate()));
+            up.setString(4, getSerializedCharacterString());
             up.setInt(5, getId());
             up.executeUpdate();
         } catch (Exception e) {
