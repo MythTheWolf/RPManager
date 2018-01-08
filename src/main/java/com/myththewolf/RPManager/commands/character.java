7d25513e4323727bb2a0f6558cc924deff738431
MythTheWolf <myth.wolf6@gmail.com>
2018-01-07 22:06:50 -0600

    Add string search to characters

--- src/main/java/com/myththewolf/RPManager/commands/character.java
+++ src/main/java/com/myththewolf/RPManager/commands/character.java
@@ -62,6 +62,46 @@ public class character implements CommandExecutor {
                 reply.setThumbnail(c.getReferenceImages().get(0));
             }
             discordCommand.e.getTextChannel().sendMessage(reply.build()).queue();
+        } else {
+            boolean didthedo = false;
+            RolePlayCharacter c = null;
+            try {
+                String build = "";
+                for (String S : discordCommand.getArgs()) {
+                    build += S + " ";
+                }
+                PreparedStatement ps = RPManagerLoader.getSQLConnection().prepareStatement("SELECT * FROM `Characters` WHERE `name` LIKE ?");
+                ps.setString(1, build + "%");
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    didthedo = true;
+                    c = new RolePlayCharacter(Integer.parseInt(discordCommand.getArgs()[0]));
+                }
+                if (!didthedo) {
+                    discordCommand.reply(":warning: No character found by or like that name.");
+                    return;
+                }
+                EmbedBuilder reply = new EmbedBuilder();
+                reply.setColor(Color.GREEN);
+                reply.setTitle("Character Info - " + c.getName());
+                reply.addField("**Name**", "```" + c.getName() + "```", false);
+                reply.addField("**Species**", "```" + c.getSpecies() + "```", false);
+                reply.addField("**Height**", "```" + c.getHeight() + "```", false);
+                reply.addField("**Gender/Pronouns**", "```" + c.getGender() + "```", false);
+                reply.addField("**Color patterns**", "```" + c.getColors() + "```", false);
+                reply.addField("**Sexual Preference**", "```" + c.getSexuality() + "```", false);
+                reply.addField("**Bio**", "```" + c.getBio() + "```", false);
+                reply.addField("**Additional Notes**", "```" + c.getNotes() + "```", false);
+                reply.setFooter("This character is property of " + c.getCharacterOwner().asRawDiscordUser().getName() + "#" + c.getCharacterOwner().asRawDiscordUser().getDiscriminator(), c.getCharacterOwner().asRawDiscordUser().getAvatarUrl());
+                if (c.getReferenceImages().size() < 1) {
+                    reply.setThumbnail("https://vignette.wikia.nocookie.net/janethevirgin/images/4/42/Image-not-available_1.jpg/revision/latest?cb=20150721102313");
+                } else {
+                    reply.setThumbnail(c.getReferenceImages().get(0));
+                }
+                discordCommand.e.getTextChannel().sendMessage(reply.build()).queue();
+            } catch (SQLException e) {
+                RPManagerLoader.LogError(e);
+            }
         }
     }
 
