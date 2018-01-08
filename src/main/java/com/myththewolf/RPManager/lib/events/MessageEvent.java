3fd2e9708f430cd625eed4448a27cada179b50a6
MythTheWolf <myth.wolf6@gmail.com>
2018-01-08 05:04:14 -0600

    fix NPE at the message event?

--- src/main/java/com/myththewolf/RPManager/lib/events/MessageEvent.java
+++ src/main/java/com/myththewolf/RPManager/lib/events/MessageEvent.java
@@ -36,6 +36,8 @@ public class MessageEvent implements EventListener {
         DiscordUser user = DataCache.getDiscordUserByID(event.getAuthor().getId());
         found = false;
         target = null;
+        DataCache.clearRPCache();
+        RPManagerLoader.storeAllRPS();
         DataCache.getRoleplayMap().forEach((id, rp) -> {
             if (rp.getHostChannel().getId().equals(event.getTextChannel().getId())) {
                 found = true;
