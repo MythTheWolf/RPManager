49ed5f71a82c5afa0e4ac684ef24e9701fc2fa72
MythTheWolf <myth.wolf6@gmail.com>
2018-01-31 07:49:00 -0600

    Remove old test command

--- src/main/java/com/myththewolf/RPManager/lib/RolePlay/RolePlayBuilder.java
+++ src/main/java/com/myththewolf/RPManager/lib/RolePlay/RolePlayBuilder.java
@@ -1,8 +1,8 @@
 package com.myththewolf.RPManager.lib.RolePlay;
 
 import com.myththewolf.RPManager.RPManagerLoader;
-import com.myththewolf.RPManager.lib.RolePlay.Character.RolePlayCharacter;
 import com.myththewolf.RPManager.lib.DataCache;
+import com.myththewolf.RPManager.lib.RolePlay.Character.RolePlayCharacter;
 import com.myththewolf.RPManager.lib.User.DiscordUser;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
